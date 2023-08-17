d5534e1508e1d632235adad3d0acc8f50a7bb311
loknathbanerjee <lokabanerjee@gmail.com>
2023-08-17 20:33:11 +0530

    component data transfer using service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccountComponent } from './account/account.component';
+import { NewAccountComponent } from './new-account/new-account.component';
+import { AccountService } from './account.service';
+import { LoggingService } from './logging.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountComponent,
+    NewAccountComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [AccountService, LoggingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/new-account/new-account.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { LoggingService } from '../logging.service';
+import { AccountService } from '../account.service';
+
+@Component({
+  selector: 'app-new-account',
+  templateUrl: './new-account.component.html',
+  styleUrls: ['./new-account.component.css']
+})
+export class NewAccountComponent {
+
+
+  constructor(private accountService: AccountService){
+    this.accountService.statusUpdate.subscribe(
+      (status: string) => alert('New Status ' + status)
+    )
+  }
+
+  onCreatAccount(accountName: string, accountStatus: string){
+    this.accountService.addAccount(accountName,accountStatus)  
+  }
+}
